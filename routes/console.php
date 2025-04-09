51f3da31f354ef4b3319aac5090c536f2243551b
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-09 15:52:03 +0800

    add command to triggered the remnder every certain time

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -17,3 +21,22 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule::call(function () {
+//     Log::info('Scheduled task ran at ' . now());
+//     // Query to find users with null email_verified_at
+//     $users = User::whereNull('email_verified_at')->get();
+//     if ($users->isEmpty()) {
+//         Log::info('No users found with null email_verified_at');
+//     } else {
+//         Log::info('Found ' . $users->count() . ' users with null email_verified_at');
+//         // Update the users' email_verified_at to the current time
+//         $users->each(function ($user) {
+//             Log::info('Updating user: ' . $user->id);
+//             $user->update(['name' => 'memek']);
+//         });
+//     }
+    
+// })->everyFiveSeconds();
+
+Schedule::command('reminders:send')->everyMinute();
