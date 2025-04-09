a6d8e38c36b5146d4563f059711f1f21e1dd151a
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-09 15:52:34 +0800

    .

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,14 +15,14 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Admin user
-        // User::create([
-        //     'email' => 'admin@example.com',
-        //     'name'=> 'Admin User',
-        //     'password' => Hash::make('password'), // Use Hash::make() to hash the password
-        //     'role' => 'admin',
-        // ]);
+        User::create([
+            'email' => 'admin@example.com',
+            'name'=> 'Admin User',
+            'password' => Hash::make('password'), // Use Hash::make() to hash the password
+            'role' => 'admin',
+        ]);
 
-          // Generate 50 organizers using the factory
+        //   Generate 50 organizers using the factory
           User::factory()->count(50)->create();
     }
 }
